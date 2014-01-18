976d93caef2fbb1c3d49a0acbdad406c9c6ea2da
Diony Rosa <dhrosa@gmail.com>
2014-01-17 23:21:03 -0500

    Implemented unary alu operations.

--- AVR/ALU.hs
+++ AVR/ALU.hs
@@ -10,10 +10,8 @@ data UnaryOpType = Complement -- ^ Flip bits
                  | Negate     -- ^ Two's complement negation
                  | Increment  -- ^ Increment by 1
                  | Decrement  -- ^ Decrement by 1
-                 | Test       -- ^ Check if non-zero
-                 | Clear      -- ^ Set to zero
                  | Set        -- ^ Set register to 0xFF
-                 deriving (Eq, Show)
+                 deriving (Eq, Enum, Show)
 
 -- | Binary ALU Operations
 data BinaryOpType = Add
@@ -23,7 +21,7 @@ data BinaryOpType = Add
                   | And
                   | Or
                   | Xor
-                  deriving (Eq, Show)
+                  deriving (Eq, Enum, Show)
                            
 data AluOp = NoOp |
              UnaryOp { unaryOpType :: UnaryOpType,
@@ -44,9 +42,36 @@ data AluResult = AluResult {
                     
 emptyResult :: AluResult
 emptyResult = AluResult 0 S.empty
-             
+
+defaultUpdate :: S.StatusReg -> Bool -> Word8 -> S.StatusReg
+defaultUpdate s v val = s {S.zero     = z,
+                           S.negative = n,
+                           S.overflow = v,
+                           S.sign     = n /= v}
+  where
+    z = val == 0
+    n = testBit val 7
+
 alu :: AluOp -> AluResult
-alu NoOp = AluResult 0 S.empty
+alu NoOp = emptyResult
 
 alu (UnaryOp op a s) = case op of
-  _ -> emptyResult
+  Complement -> let val = complement a
+                in AluResult val $ (defaultUpdate s False val) {S.carry = True}
+                   
+  Negate     -> let val = -a
+                    v = and ((testBit val 7) : map (not . testBit val) [0..6])
+                    c = or (map (testBit val) [0..7])
+                in AluResult val $ (defaultUpdate s v val) {S.carry = c}
+                   
+  Increment  -> let val = a + 1
+                    v = and ((testBit val 7) : map (not . testBit val) [0..6])
+                in AluResult val $ (defaultUpdate s v val)
+                   
+  Decrement  -> let val = a - 1
+                    v   = and ((not (testBit val 7)) : map (testBit val) [0..6])
+                in AluResult val $ (defaultUpdate s v val)
+                   
+  Set        -> AluResult 0xFF s
+                    
+  _          -> emptyResult
--- Main.hs
+++ Main.hs
@@ -5,4 +5,9 @@ import qualified AVR.StatusReg as S
 import qualified AVR.ALU as A
 
 main :: IO()
-main = print (S.set (S.empty) 4 True)
+main = do
+  let
+    opTypes = enumFromTo A.Complement A.Set
+    val = 0xAA
+    ops = map (\t -> (t, A.alu (A.UnaryOp t val S.empty))) opTypes
+  putStrLn . unlines . map show $ ops
