72a4b63a349ffc9798ef05c4959f9ca215441da8
Diony Rosa <dhrosa@gmail.com>
2014-01-20 01:40:04 -0500

    Added cycles incrementing to exec.

--- AVR/Exec.hs
+++ AVR/Exec.hs
@@ -21,13 +21,16 @@ data SRegUpdate = NoSRegUpdate
                 | SRegUpdate
                   deriving (Eq, Show)
 
+newtype Cycles = Cycles { getCycles :: Integer }
+
 exec :: Instruction -> State -> State
-exec inst state@State{programCounter=pc, regFile=rf, sreg=s}
+exec inst state@State{programCounter=pc, regFile=rf, sreg=s, cycles=oldCycles}
   = state { programCounter = nextPC,
             regFile = newRegFile,
             sreg = newSreg,
             halted = inst == HALT,
-            skipInstruction = pcUpdate == PCSkip
+            skipInstruction = pcUpdate == PCSkip,
+            cycles = oldCycles + getCycles cyclesInc
           }
   where
     reg num = R.getReg num rf
@@ -48,165 +51,195 @@ exec inst state@State{programCounter=pc, regFile=rf, sreg=s}
       PCSkip     -> pc + 1
       PCOffset k -> pc + k + 1
     
-    (aluOp, rfUpdate, sregUpdate, pcUpdate) = case inst of
+    (aluOp, rfUpdate, sregUpdate, pcUpdate, cyclesInc) = case inst of
       -- Arithmetic operations
       ADD  ra rb  -> (A.BinaryOp A.Add (reg ra) (reg rb) s,
                       RegFileUpdate ra,
                       SRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
                      
       ADC  ra rb  -> (A.BinaryOp A.AddCarry (reg ra) (reg rb) s,
                       RegFileUpdate ra,
                       SRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
                      
       SUB  ra rb  -> (A.BinaryOp A.Subtract (reg ra) (reg rb) s,
                       RegFileUpdate ra,
                       SRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
                      
       SUBI ra imm -> (A.BinaryOp A.Subtract (reg ra) imm s,
                       RegFileUpdate ra,
                       SRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
                                   
       SBC  ra rb  -> (A.BinaryOp A.SubtractCarry (reg ra) (reg rb) s,
                       RegFileUpdate ra,
                       SRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
 
       SBCI ra imm -> (A.BinaryOp A.SubtractCarry (reg ra) imm s,
                       RegFileUpdate ra,
                       SRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
 
       AND  ra rb  -> (A.BinaryOp A.And (reg ra) (reg rb) s,
                       RegFileUpdate ra,
                       SRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
 
       ANDI ra imm -> (A.BinaryOp A.And (reg ra) imm s,
                       RegFileUpdate ra,
                       SRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
 
       OR   ra rb  -> (A.BinaryOp A.Or (reg ra) (reg rb) s,
                       RegFileUpdate ra,
                       SRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
 
       ORI  ra imm -> (A.BinaryOp A.Or (reg ra) imm s,
                       RegFileUpdate ra,
                       SRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
 
       EOR  ra rb  -> (A.BinaryOp A.Xor (reg ra) (reg rb) s,
                       RegFileUpdate ra,
                       SRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
 
       COM  ra     -> (A.UnaryOp A.Complement (reg ra) s,
                       RegFileUpdate ra,
                       SRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
 
       NEG  ra     -> (A.UnaryOp A.Negate (reg ra) s,
                       RegFileUpdate ra,
                       SRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
 
       INC  ra     -> (A.UnaryOp A.Increment (reg ra) s,
                       RegFileUpdate ra,
                       SRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
 
       DEC  ra     -> (A.UnaryOp A.Decrement (reg ra) s,
                       RegFileUpdate ra,
                       SRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
 
       SER  ra     -> (A.UnaryOp A.Set (reg ra) s,
                       RegFileUpdate ra,
                       SRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
                      
       -- Branch instructions
       RJMP k       -> (A.NoOp,
                        NoRegFileUpdate,
                        SRegUpdate,
-                       PCOffset k)
+                       PCOffset k,
+                       Cycles 2)
       
       CP   ra rb  -> (A.BinaryOp A.Subtract (reg ra) (reg rb) s,
                       NoRegFileUpdate,
                       SRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
                      
       CPC  ra rb  -> (A.BinaryOp A.SubtractCarry (reg ra) (reg rb) s,
                       NoRegFileUpdate,
                       SRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
                      
       CPI  ra imm -> (A.BinaryOp A.SubtractCarry (reg ra) imm s,
                       NoRegFileUpdate,
                       SRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
                      
       -- Data Transfer
                      
       MOV  ra rb  -> (A.UnaryOp A.Identity (reg rb) s,
                       RegFileUpdate ra,
                       NoSRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
                      
       LDI  ra imm -> (A.UnaryOp A.Identity imm s,
                       RegFileUpdate ra,
                       NoSRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
                      
       -- Bit Ops
       
       LSR  ra     -> (A.UnaryOp A.LogicalShiftRight (reg ra) s,
                       RegFileUpdate ra,
                       SRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
                      
       ROR  ra     -> (A.UnaryOp A.RotateRight (reg ra) s,
                       RegFileUpdate ra,
                       SRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
                      
       ASR  ra     -> (A.UnaryOp A.ArithmeticShiftRight (reg ra) s,
                       RegFileUpdate ra,
                       SRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
                      
       SWAP ra     -> (A.UnaryOp A.Swap (reg ra) s,
                       RegFileUpdate ra,
                       NoSRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
                      
       BCLR ind    -> (A.BitOp A.FlagClear undefined ind s,
                       NoRegFileUpdate,
                       SRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
                      
       BSET ind    -> (A.BitOp A.FlagSet undefined ind s,
                       NoRegFileUpdate,
                       SRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
                      
       BST  ra ind -> (A.BitOp A.StoreTransfer (reg ra) ind s,
                       NoRegFileUpdate,
                       SRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
                      
       BLD  ra ind -> (A.BitOp A.LoadTransfer (reg ra) ind s,
                       RegFileUpdate ra,
                       NoSRegUpdate,
-                      PCNext)
+                      PCNext,
+                      Cycles 1)
                      
       -- MCU Control
                      
-      NOP         -> (A.NoOp, NoRegFileUpdate, NoSRegUpdate, PCNext)
+      NOP         -> (A.NoOp, NoRegFileUpdate, NoSRegUpdate, PCNext, Cycles 1)
                      
-      HALT        -> (A.NoOp, NoRegFileUpdate, NoSRegUpdate, PCStall)
+      HALT        -> (A.NoOp, NoRegFileUpdate, NoSRegUpdate, PCStall, Cycles 1)
