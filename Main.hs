284923b81c658a9dd5dac9786113227b4387dbcc
Diony Rosa <dhrosa@gmail.com>
2014-01-21 17:10:57 -0500

    Added a REPL in main.

--- Main.hs
+++ Main.hs
@@ -12,7 +12,12 @@ import Data.Bits
 import Data.Word (Word8,  Word16)
 import Control.Monad
 
+import System.Console.Readline
+import System.Exit
 import System.Process (system)
+import System.Environment (getArgs)
+
+import Text.Printf (printf)
 
 -- | Re-interpets a list of Word8 (as in from a ByteString unpack) as a list of Word16
 word8to16 :: [Word8] -> [Word16]
@@ -24,13 +29,20 @@ word8to16 _ = error "input list must have even number of bytes."
 
 type Zipper a = ([a], [a])
 
-forward :: Zipper a -> Zipper a
-forward ([], bs) =  ([], bs)
-forward (a:as, bs) = (as, a:bs)
+toZipper :: [a] -> Zipper a
+toZipper x = (x, [])
 
-back ::  Zipper a -> Zipper a
-back (as, b:bs) = (b:as, bs) 
-back (as, []) = (as, [])
+forward :: Zipper a -> Maybe (Zipper a)
+forward ([], _) = Nothing
+forward (a:as, bs) = Just (as, a:bs)
+
+back ::  Zipper a -> Maybe (Zipper a)
+back (_, []) = Nothing
+back (as, b:bs) = Just (b:as, bs) 
+
+current :: Zipper a -> a
+current (a:_, _) = a
+current _  = error "Cannot take current value of empty zipper."
 
 -- | Executes one simulation step
 -- | The return value is a tuple of the instruction executed, and the next state
@@ -43,16 +55,49 @@ stepUntilDone :: State -> [(Instruction, State)]
 stepUntilDone initial
   = tail $ takeWhile (not . halted . snd) $ iterate (step . snd) (NOP, initial)
     
+-- | REPL (read-evaluate-print-loop) for simulator
+simulate :: Zipper (Instruction, State) -> IO (Zipper (Instruction, State))
+simulate steps = do
+  let (inst, state) = current steps
+      again = (simulate steps)
+  
+  putStrLn ""
+  putStrLn $ printf "PC = 0x%04X" (oldProgramCounter state)
+  print inst
+  putStrLn ""
+  line  <- readline "> "
+  
+  case line of
+    Nothing -> exitSuccess
+    Just command -> 
+      case command of 
+        "regs" -> addHistory command >> print (regFile state) >> again
+      
+        "back" -> case (back steps) of
+          Nothing   -> (putStrLn "Cannot backtrack any further.") >> again
+          Just prev -> addHistory command >> simulate prev
+        
+        "step" -> case (forward steps) of
+          Nothing -> (putStrLn "Cannot step any further.") >> again
+          Just next -> addHistory command >> simulate next
+      
+        "quit" -> exitSuccess
+
+        _      -> (putStrLn "Unrecognized command.") >> again
+
 -- | Takes an assembler source frmo stdin and simulates it.
 main :: IO()
 main = do
   -- assemble the input
-  assemblySource <- getContents
-  writeFile "temp.s" assemblySource
+  source <- liftM head getArgs
+  _ <- system ("cp " ++ source ++ " temp.s")
   _ <- system "avr-as -mmcu=avr1 temp.s -o temp.a"
   _ <- system "avr-ld -mavr1 -Tlinker.x -o temp.elf temp.a"
   _ <- system "avr-objcopy -S -O binary temp.elf temp.bin"
   pmem <- liftM (word8to16 . B.unpack) (B.readFile "temp.bin")
   _ <- system "rm -f temp.s temp.a temp.elf temp.bin"
 
-  print $  stepUntilDone (initialState pmem)
+  --hSetBuffering stdout NoBuffering
+
+  _ <- simulate $ toZipper $ stepUntilDone (initialState pmem)
+  return ()
