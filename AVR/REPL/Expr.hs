b54751750dd615f12b6a9c70739a6459f3c94902
Diony Rosa <dhrosa@gmail.com>
2014-02-04 10:41:42 -0500

    Added parsing of some 8-bit expressions.

--- AVR/REPL/Expr.hs
+++ AVR/REPL/Expr.hs
@@ -10,8 +10,12 @@ import Control.Applicative
 
 import Control.Monad
 import Numeric (readHex)
+
 import Text.Parsec.String
+import Text.Parsec.Char
+import Text.Parsec.Combinator
 import Text.Parsec.Expr
+import Text.Parsec.Prim ((<?>), try)
 
 data Expr a where
   -- Basic Units
@@ -34,6 +38,65 @@ data Expr a where
   Add :: (Num a) => Expr a -> Expr a -> Expr a
   Subtract :: (Num a) => Expr a -> Expr a -> Expr a
   
+-- | Parses a decimal value
+decimal :: (Integral a, Read a) => Parser a
+decimal = read <$> many1 digit
+          <?> "decimal value"
+
+-- | Parses a hexadecimal value
+hex :: (Integral a, Read a) => Parser a
+hex = do
+  _ <- string "0x"
+  [(val, "")] <- readHex <$> many1 hexDigit
+  return val
+  <?> "hexadecimal value"
+  
+-- | Parses an integral literal value
+literal :: (Integral a, Read a) => Parser a
+literal = try hex <|> decimal
+          <?> "literal"
+  
+-- | Parses an 8-bit literal
+lit8 :: Parser (Expr Word8)
+lit8 = do
+  num <- literal :: Parser Int
+  guard (num <= 0xFF)
+  return $ Lit8 num
+  <?> "8-bit literal"
+  
+-- | Parses a 16-bit literal
+lit16 :: Parser (Expr Word16)
+lit16 = do
+  num <- literal :: Parser Int
+  guard (num <= 0xFFFF)
+  return $ Lit16 num
+
+-- | Parses a register
+reg8 :: Parser (Expr Word8)
+reg8 = do
+  _ <- oneOf "Rr"
+  num <- read <$> many1 digit
+  return (Reg (toEnum num))
+  <?> "register"
+
+-- | Parses an 8-bit expression
+expr8 :: Parser (Expr Word8)
+expr8 = buildExpressionParser opTable term
+  where
+    binOp name func assoc = Infix (string name >> return func) assoc
+    prefixOp name func = Prefix (string name >> return func)
+--    postfixOp name func = Postfix (string name >> return func)
+                      
+    opTable = [
+      [prefixOp "D@" DMem],
+      [binOp "+" Add AssocLeft, binOp "-" Subtract AssocLeft]
+      ]
+              
+    term = foldl1 (<|>) [
+      lit8,
+      reg8
+      ]
+           
 -- | Evaluates the value of an expression in the context of the current processor state.
 eval :: Expr a -> AVRState -> a
 eval (Lit8 n) = const (fromIntegral n)
