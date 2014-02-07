80e875a0611d8348e42105a88c8f4ecb27f15fcc
Diony Rosa <dhrosa@gmail.com>
2014-02-06 23:05:26 -0500

    Replaced Zipper with a simple list of past AVRStates.

--- AVR/REPL.hs
+++ AVR/REPL.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE GADTs #-}
 module AVR.REPL where
 
-import AVR.Decoder (decode, Instruction (NOP))
+import AVR.Decoder (decode, Instruction)
 import AVR.Fetch (fetch)
 import AVR.Exec (exec)
 import AVR.AVRState
@@ -14,51 +14,41 @@ import System.Exit
 
 import AVR.REPL.Command
 import AVR.REPL.Expr
-import AVR.REPL.Zipper
 
 import Control.Monad
 import Text.Parsec (parse)
 
-type REPLState = Zipper (Instruction, AVRState)
 type EvalError = String
+
+fetchDecode :: AVRState -> Instruction
+fetchDecode = decode . fetch
 
 -- | Executes one simulation step
 -- | The return value is a tuple of the instruction executed, and the next state
-step :: AVRState -> (Instruction, AVRState)
-step state = let inst = decode (fetch state)
-             in (inst, exec inst state)
+step :: AVRState -> AVRState
+step = exec =<< fetchDecode
 
--- | Takes an initial processor state and simulates it until the processor halts.
-stepUntilDone :: AVRState -> [(Instruction, AVRState)]
-stepUntilDone initial
-  = tail $ takeWhile (not . halted . snd) $ iterate (step . snd) (NOP, initial)
-
-evaluate :: String -> REPLState -> (Either EvalError String, REPLState)
-evaluate line replState = case parse parseCommand "repl" line of
-  Left err -> (Left (show err), replState)
+evaluate :: String -> [AVRState] -> (Either EvalError String, [AVRState])
+evaluate line history@(current:past) = case parse parseCommand "repl" line of
+  Left err -> (Left (show err), history)
   
   Right command -> case command of
-    Print8 expr -> (Right $ show $ eval expr state, replState)
-    Print16 expr -> (Right $ show $ eval expr state, replState)
+    Print8 expr -> (Right $ show $ eval expr current, history)
+    Print16 expr -> (Right $ show $ eval expr current, history)
     
-    Step   n    -> case (forward n replState) of
-      Nothing -> (Left "Cannot step any further.", replState)
-      Just next -> (Right (show . fst . current $ next), next)
+    Step   n    -> let newHistory = (iterate (\h -> step (head h) : h) history) !! n
+                   in (Right $ show $ fetchDecode $ head $ newHistory, newHistory)
                    
-    Back   n    -> case (back n replState) of
-      Nothing -> (Left "Cannot backtrack any further.", replState)
-      Just prev -> (Right (show . fst . current $ prev), prev)
+    Back   n    -> if n >= length history
+                   then (Left "Cannot backtrack any further.", [last history])
+                   else let newHistory = drop n history
+                        in (Right $ show $ fetchDecode $ head $ newHistory, newHistory)
       
     Set8 target val -> let updateState = case target of 
                              TargetReg num -> setReg num =<< eval val
-                           newCurrent = (inst, updateState state)
-                           newFuture = stepUntilDone . snd $ newCurrent
-                           
-                       in (Right "", updateFuture newFuture $ updateCurrent newCurrent replState)
-    where
-      (inst, state) = current replState
+                       in (Right "", updateState current : past)
 
-loop :: REPLState -> IO (REPLState)
+loop :: [AVRState] -> IO [AVRState]
 loop replState = do
   line <- readline "> "
   case line of 
@@ -74,4 +64,4 @@ iterateM :: (Monad m) => (a -> m a) -> a -> m a
 iterateM = foldr (>=>) return . repeat
   
 repl :: Vector Word16 -> IO()
-repl pmem = (iterateM loop . toZipper . stepUntilDone . initialState $ pmem) >> return ()
+repl pmem = (iterateM loop [initialState pmem]) >> return ()
